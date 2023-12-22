9be00cfc2a46147589fe0aba811d910c21eb4969
FellpsR <feliperodriguesrdo@gmail.com>
2023-12-22 17:40:56 -0300

    Update placeholder input name and guess-number (fix pending)

--- imersao-alura/dist/script.js
+++ imersao-alura/dist/script.js
@@ -2,30 +2,38 @@
 var numberSecret = Math.floor(Math.random() * 100) + 1; 
 
 var playerName;
-// while (true) {
-//   playerName = prompt("Digite seu nome:");
+while (true) {
+  playerName = request.form["name-input"];
 
-//   // Verifica se o nome contém apenas letras e caracteres especiais, sem números
-//   if (/^[A-Za-z]+$/.test(playerName)) {
-//     break; // Sai do loop se o nome estiver correto
-//   } else {
-//     alert("Por favor, digite um nome válido contendo apenas letras e caracteres especiais, sem números.");
-//   }
-// }
+  // Verifica se o nome contém apenas letras e caracteres especiais, sem números
+  if (/^[A-Za-z]+$/.test(playerName)) {
+    break; // Sai do loop se o nome estiver correto
+    
+  } else {
+    error_message = "Por favor, digite um nome válido contendo apenas letras e caracteres especiais, sem números.";
+    return render_template("index.html", error_message); // Retorna a mensagem de erro para o usuário
+  }
+}
 
-// var strike;
-// while (strike !== numberSecret) {
-//   strike = Math.floor(prompt(`${playerName}, Digite um número entre 1 e 100`));
+var strike;
+while (strike !== numberSecret) {
+  strike = Math.floor(prompt(`${playerName}, Digite um número entre 1 e 100`));
 
   //verifica se o usuário fez o input de um número
   if (isNaN(strike)) {
-    alert(`${playerName}, Por favor, digite um número válido.`);
+    error_message = `${playerName}, Por favor, digite um número válido.`;
+    return render_template("index.html", error_message);
+
   } else if (strike === numberSecret) {
     alert(`${playerName}, Você acertou na mosca, o número secreto é ${numberSecret}!`);
+
   } else if (strike <= 0){
-    alert(`${playerName}, Número inválido, Digite um número entre 1 e 100`);
+    error_message = `${playerName}, Número inválido, Digite um número entre 1 e 100`;
+    return render_template("index.html", error_message);
+
   } else if (strike > numberSecret) {
     alert(`Errooooou, o número secreto é menor do que ${strike}!`);
+
   } else {
     alert(`Errooooou, o número secreto é maior do que ${strike}!`);
   }
